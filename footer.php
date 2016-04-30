6f60155472281c3b0f9a360f7661d4f8d2e3c9d0
Joseph <josasheraden@gmail.com>
2016-04-29 20:49:49 -0500

    Started on MyTheme2

---
+++ footer.php
@@ -0,0 +1,16 @@
+	  </div>
+	</div>
+  	<div class="site-footer">
+	  <nav class="navbar navbar-inverse navbar-static-bottom">
+	    <div class="container">
+	      <div class="navbar-header">
+	        <span id="copyright" class="navbar-brand"></span>
+	      </div>
+	    </div>
+	  </nav>
+    </div>
+  </div>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<div class="posts-wrapper"><?php post_loop(get_post_format()); ?></div>
+<?php get_footer(); ?>
