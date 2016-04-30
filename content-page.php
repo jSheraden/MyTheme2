cee0880ef88958801c1eb2b17ddb2c8cd49f4619
Joseph <josasheraden@gmail.com>
2016-04-29 22:49:12 -0500

    Changed some markup and edited stylesheet

---
+++ content-page.php
@@ -0,0 +1,4 @@
+<div class="post">
+  <h2 class="page-title"><?php echo get_the_title(); ?></h2>
+  <h4><?php the_content(); ?></h4>
+</div>
---
+++ page.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<div class="page-content"><?php post_loop('page'); ?></div>
+<?php get_footer(); ?>
